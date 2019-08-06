61aeabcaadf2895fa71d943b9fca2521828135e7
priyanka-kale <kalepriya100@gmail.com>
2019-08-06 12:17:32 +0530

    sort by function

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./app'); // app file include
-const port = process.env.PORT || 5006;
-//const port = process.env.PORT || 3057;
+//const port = process.env.PORT || 5006;
+const port = process.env.PORT || 3057;
 
 const server = http.createServer(app);
 
