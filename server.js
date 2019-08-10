1455900977eda92fbc79b67fa7d94be36272dfc4
priyanka-kale <kalepriya100@gmail.com>
2019-08-10 23:46:03 +0530

    Priyanka kale added subarea, society related changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./app'); // app file include
-const port = process.env.PORT || 3002;
-//const port = process.env.PORT || 3057;
+//const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3057;
 
 const server = http.createServer(app);
 
