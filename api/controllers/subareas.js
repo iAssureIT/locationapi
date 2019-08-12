7ae55c1c06547e307cff13599c597052c0ec825b
ashishnaik021 <iassureit@gmail.com>
2019-08-12 19:36:09 +0530

    Ashish Naik

--- api/controllers/subareas.js
+++ api/controllers/subareas.js
@@ -31,7 +31,8 @@ exports.insertSubarea = (req,res,next)=>{
 
 exports.getSubAreas = (req,res,next)=>{
     console.log(req.params);
-    SubAreas  .find({
+        SubAreas  
+            .find({
                 "stateCode"        :   { "$regex": req.params.stateCode, $options: "i"},
                 "districtName"     :   { "$regex": req.params.districtName, $options: "i"},
                 "blockName"        :   { "$regex": req.params.blockName, $options: "i"},
@@ -39,14 +40,14 @@ exports.getSubAreas = (req,res,next)=>{
                 "areaName"         :   { "$regex": req.params.areaName, $options: "i" },
                 "status"           :   "approved"
             })
-            .sort({ "societyName": 1 })
+            .sort({ "subareaName": 1 })
             .exec()
             .then(data=>{
                 if(data.length>0){
                     console.log("getSubAreas data = ", data);
                     res.status(200).json(data);
                 }else{
-                    res.status(200).json({"message" : 'SubAreas not found for this '+ req.params.districtName +' district and '+req.params.blockName+' block'});
+                    res.status(200).json({"message" : 'SubAreas not found for '+ req.params.cityName +' district, '+req.params.areaName+' block',});
                 }
             })
             .catch(err =>{
@@ -91,4 +92,34 @@ exports.update_status = (req,res,next)=>{
             });
         });
 };
+
+
+
+
+exports.searchSubAreas = (req,res,next)=>{
+    console.log(req.params);
+    SubAreas  .find({
+                $or : [
+                    {"cityName"         :   { "$regex": /^req.params.cityName/, $options: "i"} },
+                    {"areaName"         :   { "$regex": /^req.params.areaName/, $options: "i"} },
+                    {"subareaName"      :   { "$regex": /^req.params.subareaName/, $options: "i"} },
+                ],
+                "status"           :   "approved"
+            })
+            .exec()
+            .then(data=>{
+                if(data.length>0){
+                    console.log("getSubAreas data = ", data);
+                    res.status(200).json(data);
+                }else{
+                    res.status(200).json({"message" : 'SubAreas not found for this '+ req.params.districtName +' district and '+req.params.blockName+' block'});
+                }
+            })
+            .catch(err =>{
+                console.log(err);
+                res.status(500).json({
+                    error: err
+                });
+            });
+}
 
