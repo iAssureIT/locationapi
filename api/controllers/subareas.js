1af0e557273f9c3aa595e24935345de28e27f202
ashishnaik021 <iassureit@gmail.com>
2019-08-12 09:35:33 +0530

    Ashish Naik, making changes

--- api/controllers/subareas.js
+++ api/controllers/subareas.js
@@ -31,17 +31,18 @@ exports.insertSubarea = (req,res,next)=>{
 
 exports.getSubAreas = (req,res,next)=>{
     console.log(req.params);
-    SubAreas  .find(
-            {
+    SubAreas  .find({
                 "stateCode"        :   { "$regex": req.params.stateCode, $options: "i"},
                 "districtName"     :   { "$regex": req.params.districtName, $options: "i"},
                 "blockName"        :   { "$regex": req.params.blockName, $options: "i"},
                 "cityName"         :   { "$regex": req.params.cityName, $options: "i"},
                 "areaName"         :   { "$regex": req.params.areaName, $options: "i" }               
-            }).sort({ "societyName": 1 })
+            })
+            .sort({ "societyName": 1 })
             .exec()
             .then(data=>{
                 if(data.length>0){
+                    console.log("getSubAreas data = ", data);
                     res.status(200).json(data);
                 }else{
                     res.status(200).json({"message" : 'SubAreas not found for this '+ req.params.districtName +' district and '+req.params.blockName+' block'});
