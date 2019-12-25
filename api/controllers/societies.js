58807428bff68971379d29bc77d4f82ee73f4e33
Rushikesh Salunkhe <rushikeshsalunkhe101@gmail.com>
2019-12-25 17:34:39 +0530

    Rushikesh changed in unapproved list api

--- api/controllers/societies.js
+++ api/controllers/societies.js
@@ -134,7 +134,7 @@ exports.getUnapprovedSociety = (req,res,next)=>{
                        console.log("error=>",error);
                     });
                 }
-                if(k === unApprovedSocieties.length){
+                if(i === unApprovedSocieties.length){
                     console.log("dataList",dataList);
                     res.status(200).json(dataList);
                 }
